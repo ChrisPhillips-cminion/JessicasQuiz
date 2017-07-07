dcb79da9e070c344205cae34c4fb5a0b364d141a
Chris.Phillips <chris.phillips@uk.ibm.com>
2017-07-07 19:37:31 +0100

    Jessia

---
+++ quiz.js
@@ -0,0 +1,35 @@
+var sleep = require('sleep').sleep;
+var color = require('colors');
+j=1
+console.log("Queen quiz - by Jessica".bold)
+// sleep(10)
+console.log("")
+console.log(j++ + "\tHow many dogs does the Queen have?".yellow)
+console.log("")
+// sleep(5)
+console.log(j++ + "\tHow many grandchildren does the Queen have?");
+console.log("")
+// sleep(5)
+console.log(j++ + "\tHow many castles does the Queen have?".magenta);
+console.log("")
+// sleep(5)
+console.log(j++ + "\tHow many princesses does the Queen have?".blue);
+console.log("")
+// sleep(5)
+console.log(j++ + "\tHow many types of dogs does the Queen have?".green);
+console.log("")
+
+console.log(j++ + "\tHow many princes deos the Queen have?".cyan);
+console.log("")  
+
+console.log(j++ + "\tWho is the Queen's Great Great Grandmother?".magenta);
+console.log("")
+
+console.log(j++ + "\tWho is the Queen's Great Grandmother?");
+console.log("")
+
+console.log(j++ + "\tHow old is the Queen?".rainbow.bold);
+console.log("")
+
+console.log(j++ + "\tWho is the Queen Mother?".magenta);
+console.log("")
